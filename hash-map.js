2f953f786d305cd81fcaa3bbfaab5bbbca42f488
miguelruizc <buumchakalaka@gmail.com>
2024-05-22 11:25:54 +0200

    Add solution

--- hash-map.js
+++ hash-map.js
@@ -1,36 +1,123 @@
 class HashMap {
-	constructor() {}
+	static INITIAL_CAPACITY = 16;
+	static LOAD_FACTOR = 0.75;
+
+	constructor() {
+		this.arraySize = HashMap.INITIAL_CAPACITY;
+		this.keyCount = 0;
+
+		this.array = new Array(this.arraySize);
+		for (let i = 0; i < this.arraySize; i++) this.array[i] = {};
+	}
 
 	hash(key) {
 		let hashCode = 0;
 
 		const primeNumber = 31;
 		for (let i = 0; i < key.length; i++) {
-			hashcode = primeNumber * hashCode + key.charCodeAt(i);
+			hashCode = primeNumber * hashCode + key.charCodeAt(i);
 		}
 
-		return hashCode;
+		return hashCode % this.arraySize;
 	}
 
-	set(key, value) {}
+	set(key, value) {
+		const index = this.hash(key);
+		this.array[index][key] = value;
 
-	get(key) {}
+		this.keyCount++;
+		this.checkCapacity();
+	}
 
-	has(key) {}
+	get(key) {
+		const index = this.hash(key);
+		if (this.array[index][key]) return this.array[index][key];
+		else return null;
+	}
 
-	remove(key) {}
+	has(key) {
+		const index = this.hash(key);
+		if (this.array[index][key]) return true;
+		return false;
+	}
 
-	length() {}
+	remove(key) {
+		const index = this.hash(key);
 
-	clear() {}
+		if (this.array[index][key]) {
+			delete this.array[index][key];
+			this.keyCount--;
+			return true;
+		}
 
-	keys() {}
+		return false;
+	}
 
-	values() {}
+	length() {
+		return this.keyCount;
+	}
 
-	entries() {}
+	reset(size = HashMap.INITIAL_CAPACITY) {
+		this.arraySize = size;
+		this.keyCount = 0;
+
+		this.array = new Array(this.arraySize);
+		for (let i = 0; i < this.arraySize; i++) this.array[i] = {};
+	}
+
+	keys() {
+		let keys = [];
+		this.array.forEach((element) => {
+			keys.push(...Object.keys(element));
+		});
+
+		return keys;
+	}
+
+	values() {
+		let values = [];
+		this.array.forEach((element) => {
+			values.push(...Object.values(element));
+		});
+
+		return values;
+	}
+
+	entries() {
+		let entries = [];
+		this.array.forEach((element) => {
+			entries.push(...Object.entries(element));
+		});
+
+		return entries;
+	}
+
+	checkCapacity() {
+		let currentLoad = this.keyCount / this.arraySize;
+
+		if (currentLoad >= HashMap.LOAD_FACTOR) this.grow();
+	}
+
+	grow() {
+		// Store data
+		let keys = this.keys();
+		let values = this.values();
+		let keyCount = this.keyCount;
+
+		// Expand array
+		this.arraySize += 16;
+		this.reset(this.arraySize);
+		
+		// Add data back
+		for(let i = 0; i < keyCount; i++){
+			this.set(keys[i], values[i]);
+		}
+	}
 }
 
-class HashSet {
-	//Create a HashSet class or factory function that behaves the same as a HashMap but only contains keys with no values.
+// Test
+let h = new HashMap();
+for (let i = 0; i < 30; i++) {
+	h.set(`key${i}`, `value${i}`);
+	console.log(h);
 }
